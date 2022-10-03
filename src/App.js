01ee0d4cbfcf76d1f2dda8b902fa007f4408cb20
AdrienB75 <adrien.biguet@ibm.com>
2022-10-03 14:00:57 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi there
+          Edit <code>src/App.js</code> I love Docker
         </p>
         <a
           className="App-link"
